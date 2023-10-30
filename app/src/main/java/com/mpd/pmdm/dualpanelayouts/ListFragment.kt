6cf245175182c7ce5b4813e4fcb0d19c6e7af9f3
mcventur <marcelinopenide@gmail.com>
2023-10-30 18:07:28 +0100

    Añadido segundo fragmento

--- app/src/main/java/com/mpd/pmdm/dualpanelayouts/ListFragment.kt
+++ app/src/main/java/com/mpd/pmdm/dualpanelayouts/ListFragment.kt
@@ -1,9 +1,11 @@
 package com.mpd.pmdm.dualpanelayouts
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
@@ -11,15 +13,27 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ListFragment : Fragment() {
+//Implentamos View.OnClickListener para que sea clickable el fragmento
+class ListFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    //Declaramos una variable para almacenar la implementación de la interfaz
+    //StarSignListener declarada en MainActivity.kt
+    private lateinit var starSignListener: StarSignListener
+
+    //Con este onAttach comprobamos que la Actividad que contenga este fragmento
+    //implemente el starSignListener, o de lo contrario se lanzará una excepción.
+    //Así aseguramos poder notificar a la Actividad cuando se haga click en un signo
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is StarSignListener){
+            starSignListener = context
+        } else{
+          throw RuntimeException("Must implement StarSignListener")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +49,46 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val starSigns = listOf<View>(
+            view.findViewById(R.id.aquarius),
+            view.findViewById(R.id.pisces),
+            view.findViewById(R.id.aries),
+            view.findViewById(R.id.taurus),
+            view.findViewById(R.id.gemini),
+            view.findViewById(R.id.cancer),
+            view.findViewById(R.id.leo),
+            view.findViewById(R.id.virgo),
+            view.findViewById(R.id.libra),
+            view.findViewById(R.id.scorpio),
+            view.findViewById(R.id.sagittarius),
+            view.findViewById(R.id.capricorn),
+        )
+
+        //Asociamos a cada TextView de Signo el propio fragment como onClickListener (porque lo implementamos)
+        starSigns.forEach{
+            it.setOnClickListener(this)
+        }
+
+    }
+
+
+    /**
+     * Pasa el id del signo clickado al starSignListener de la Actividad (Contexto) contenedora del fragment
+     * Los Listener son un mecanismo habitual para reaccionar a cambios de otras clases.
+     * Más info: https://hinchman-amanda.medium.com/the-listener-pattern-in-android-a2c532dd2938
+     */
+    override fun onClick(v: View?) {
+        v?.let{
+            //starSignListener es el atributo declarado como lateinit en esta clase
+            //que se inicializa en el onAttach con el contexto (la actividad contenedora del fragmento)
+            starSign -> starSignListener.onSelected(starSign.id)
+        }
     }
 
     companion object {
@@ -56,4 +110,5 @@ class ListFragment : Fragment() {
                 }
             }
     }
+
 }
--- app/src/main/java/com/mpd/pmdm/dualpanelayouts/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/dualpanelayouts/MainActivity.kt
@@ -3,9 +3,24 @@ package com.mpd.pmdm.dualpanelayouts
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Declaramos nuestro propio Listener. Una interface para recibir las notificaciones
+ * al seleccionar cualquier signo del zodiaco
+ */
+interface StarSignListener{
+    fun onSelected(id: Int)
+}
+//Implementamos con MainActivity el StarSignListener
+
+class MainActivity : AppCompatActivity(), StarSignListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSelected(id: Int) {
+        TODO("Not yet implemented")
+    }
 }
+
+
